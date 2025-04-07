b28f2bd4526cfa8ddca2ff23f5736f59d6b9116c
Evan <spanglem@uc.edu>
2025-04-06 21:55:51 -0400

    Final Commit

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -1,96 +1,73 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class ArrayLists {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<String> myArrList = new ArrayList<>();
-        boolean running = true;
-
-        while (running) {
-            displayListAndMenu(myArrList);
-            String choice = SafeInput.getRegExString(in, "Choose a menu option", "^[AaDdIiPpQq]$").toUpperCase();
-
+        ArrayList<String> list = new ArrayList<>();
+        boolean quit = false;
+        do {
+            displayList(list);
+            String choice = getMenuChoice(in);
             switch (choice) {
                 case "A":
-                    addToList(myArrList);
-                    break;
-                case "D":
-                    deleteFromList(myArrList);
+                    addItem(list, in);
                     break;
                 case "I":
-                    addAtIndex(myArrList);
+                    insertItem(list, in);
+                    break;
+                case "D":
+                    deleteItem(list, in);
                     break;
                 case "P":
-                    printList(myArrList);
+                    displayList(list);
                     break;
                 case "Q":
-                    boolean confirmQuit = SafeInput.getYNConfirm(in, "Are you sure you want to quit?");
-                    if (confirmQuit) {
-                        running = false;
+                    if (SafeInput.getYNConfirm(in, "Are you sure you want to quit?")) {
+                        quit = true;
                         System.out.println("Goodbye!");
                     }
                     break;
             }
-        }
+        } while (!quit);
     }
 
-    public static void displayListAndMenu(ArrayList<String> myArrList) {
-        System.out.println("\nCurrent List:");
-        if (myArrList.isEmpty()) {
-            System.out.println("  [List is empty]");
+    public static void displayList(ArrayList<String> list) {
+        System.out.println("\n=== Current List ===");
+        if (list.isEmpty()) {
+            System.out.println("[List is empty]");
         } else {
-            displayNumberedList(myArrList);
-        }
-
-        System.out.println("\nMenu Options:");
-        System.out.println("[A] Add");
-        System.out.println("[D] Delete");
-        System.out.println("[I] Insert at Index");
-        System.out.println("[P] Print List");
-        System.out.println("[Q] Quit");
-    }
-
-    public static void displayNumberedList(ArrayList<String> list) {
-        for (int i = 0; i < list.size(); i++) {
-            System.out.printf("%d. %s%n", i + 1, list.get(i));
+            for (int i = 0; i < list.size(); i++) {
+                System.out.printf("%d. %s%n", i + 1, list.get(i));
+            }
         }
     }
 
-    public static void addToList(ArrayList<String> myArrList) {
-        Scanner in = new Scanner(System.in);
-        String item = SafeInput.getNonZeroLengthString(in, "Enter item to add to list:");
-        myArrList.add(item);
+    public static String getMenuChoice(Scanner in) {
+        System.out.println("\n=== Menu ===");
+        System.out.println("A - Add\nI - Insert\nD - Delete\nP - Print\nQ - Quit");
+        return SafeInput.getRegExString(in, "Enter your choice", "[AaIiDdPpQq]").toUpperCase();
+    }
+
+    public static void addItem(ArrayList<String> list, Scanner in) {
+        String item = SafeInput.getNonZeroLengthString(in, "Enter item to add");
+        list.add(item);
         System.out.println("Item added!");
     }
 
-    public static void deleteFromList(ArrayList<String> myArrList) {
-        Scanner in = new Scanner(System.in);
-        if (myArrList.isEmpty()) {
-            System.out.println("Nothing to delete - list is empty.");
-            return;
-        }
-        displayNumberedList(myArrList);
-        int choice = SafeInput.getRangedInt(in, "Enter item number to delete:", 1, myArrList.size());
-        myArrList.remove(choice - 1);
-        System.out.println("Item deleted!");
-    }
-
-    public static void addAtIndex(ArrayList<String> myArrList) {
-        Scanner in = new Scanner(System.in);
-        displayNumberedList(myArrList);
-        int index = SafeInput.getRangedInt(in, "Enter index to insert at (1 to " + (myArrList.size() + 1) + "):", 1, myArrList.size() + 1) - 1;
-        String item = SafeInput.getNonZeroLengthString(in, "Enter item to insert:");
-        myArrList.add(index, item);
+    public static void insertItem(ArrayList<String> list, Scanner in) {
+        int index = list.isEmpty() ? 0 : SafeInput.getRangedInt(in, "Enter position to insert at", 1, list.size() + 1) - 1;
+        String item = SafeInput.getNonZeroLengthString(in, "Enter item to insert");
+        list.add(index, item);
         System.out.println("Item inserted!");
     }
 
-    public static void printList(ArrayList<String> myArrList) {
-        if (myArrList.isEmpty()) {
-            System.out.println("The list is empty.");
+    public static void deleteItem(ArrayList<String> list, Scanner in) {
+        if (list.isEmpty()) {
+            System.out.println("List is empty. Nothing to delete.");
         } else {
-            System.out.println("Current List:");
-            displayNumberedList(myArrList);
+            int index = SafeInput.getRangedInt(in, "Enter number of item to delete", 1, list.size()) - 1;
+            String removed = list.remove(index);
+            System.out.println("Removed: " + removed);
         }
     }
 }
